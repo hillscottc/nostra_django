2139a6b2f8f24d7fa99ba81e6a02082900504072
Scott Hill <scott289@gmail.com>
2015-12-11 12:59:36 -0800

    postgres

--- nostra_proj/nostra_proj/settings.py
+++ nostra_proj/nostra_proj/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'nostra_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nostradb',
+        'USER': 'nostra_user',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'US/Pacific'
 
 USE_I18N = True
 
